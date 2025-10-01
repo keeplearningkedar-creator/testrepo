7cae9b66caa93dd829f3cd94e525b824278a0fd6
Kedar <upskillkedar@gmail.com>
2025-10-01 11:11:31 -0500

    TestCommit- added address path to endpoint

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -12,4 +12,10 @@ public class TestController {
         return name;
     }
 
+    @GetMapping("/myaddress")
+    public String getMyAddress() {
+        var name = "Aurora";
+        return name;
+    }
+
 }
